41c1df8211fd3a43baf9790461828d50384f0bc8
gz1 <guenter.zaepernick@t-online.de>
2020-06-23 19:52:34 +0200

    lotto tests added

---
+++ src/test/java/de/wbstraining/ocp/lotto/AbgabeKostenErmittelnTest.java
@@ -0,0 +1,63 @@
+package de.wbstraining.ocp.lotto;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class AbgabeKostenErmittelnTest {
+	
+	private IAbgabeKostenErmitteln kostenErmitteln;
+	private IGebuehrService gebuehrService;
+	
+	private GebuehrDto gebuehrDto;
+	private KostenErmittelnDto kostenDto;
+	
+	private final int GRUNDGEBUEHR = 60;
+	private final int EINSATZ_PRO_TIPP = 100;
+	private final int EINSATZ_SPIEL_77 = 250;
+	private final int EINSATZ_SUPER_6 = 125;
+	
+	private final int LAUFZEIT = 1;
+	private final int ANZAHL_TIPPS = 1;
+	private boolean IS_SUPER_6 = false;
+	private boolean IS_SPIEL_77 = false;
+	private boolean IS_MITTWOCH = true;
+	private boolean IS_SAMSTAG = true;
+	
+	private final LocalDate DATE = LocalDate.now();
+	
+	@BeforeEach
+	public void setupMock() {
+		kostenErmitteln = new AbgabeKostenErmitteln();
+		gebuehrService = mock(IGebuehrService.class);
+		gebuehrDto = new GebuehrDto(GRUNDGEBUEHR, EINSATZ_PRO_TIPP,
+				EINSATZ_SPIEL_77, EINSATZ_SUPER_6);
+		kostenDto = new KostenErmittelnDto(LAUFZEIT, ANZAHL_TIPPS,
+				IS_SUPER_6, IS_SPIEL_77, IS_MITTWOCH, IS_SAMSTAG);
+		kostenErmitteln.setGebuehrService(gebuehrService);
+		
+		when(gebuehrService.gebuehrFuerDatum(DATE)).thenReturn(gebuehrDto);
+	}
+	
+	@Test
+	public void kostenErmittelnTest() {
+		int expectedKosten = 160;
+		int result = kostenErmitteln.abgabeKosten(kostenDto);
+		assertTrue(expectedKosten == result);
+	}
+	
+	@Test
+	public void serviceCallTest() {
+		kostenErmitteln.abgabeKosten(kostenDto);
+		verify(gebuehrService, atLeastOnce()).gebuehrFuerDatum(DATE);
+		
+	}
+}
---
+++ src/test/java/de/wbstraining/ocp/lotto/LottoUtilTest.java
@@ -0,0 +1,74 @@
+package de.wbstraining.ocp.lotto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import de.wbstraining.ocp.lotto.LottoUtil;
+
+public class LottoUtilTest {
+
+	private static long tipp1;
+	private static long tipp2;
+	private static long tipp3;
+	private static long[] tipps;
+
+	@BeforeEach
+	public void beforeEach() {
+		System.out.println("BeforeEach");
+	}
+
+	@AfterEach
+	public void afterEach() {
+		System.out.println("AfterEach");
+	}
+
+	@BeforeAll
+	public static void beforeAll() {
+		System.out.println("BeforeAll");
+		tipp1 = LottoUtil.createTipp(1, 2, 3, 4, 5, 6);
+		tipp2 = LottoUtil.createTipp(49, 48, 47, 46, 45, 44);
+		tipp3 = LottoUtil.createTipp(10, 11, 12, 13, 14, 15);
+		tipps = new long[] { tipp1, tipp2, tipp3 };
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		System.out.println("AfterAll");
+	}
+
+	@Test
+	public void testCountMatches() {
+
+		long tipp = LottoUtil.createTipp(1, 2, 3, 4, 5, 6);
+
+		int expected = 6;
+		int actual;
+
+		actual = LottoUtil.countMatches(tipp, tipps[0]);
+
+		assertEquals(expected, actual);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 1, 2, 3 })
+	void testParameterized(int tipp) {
+		assertTrue(Long.bitCount(LottoUtil.createTipp(tipp)) == 6);
+	}
+
+	@Test
+	void testExpectedException() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			LottoUtil.createTipp(1, 2, 3, 4, 5, 6, 7);
+		});
+	}
+
+}
