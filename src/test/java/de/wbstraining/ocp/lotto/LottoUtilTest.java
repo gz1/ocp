635f8b8fca671dc1209c25c878ba415cd196ddc5
Günter <guenter.zaepernick@t-online.de>
2020-06-25 06:49:38 +0200

    m

--- src/test/java/de/wbstraining/ocp/lotto/LottoUtilTest.java
+++ src/test/java/de/wbstraining/ocp/lotto/LottoUtilTest.java
@@ -3,10 +3,6 @@ package de.wbstraining.ocp.lotto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Collections;
-import java.util.NavigableSet;
-import java.util.TreeSet;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -16,35 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import de.wbstraining.ocp.lotto.LottoUtil;
+
 public class LottoUtilTest {
 
 	private static long tipp1;
 	private static long tipp2;
 	private static long tipp3;
 	private static long[] tipps;
-
-	// more
-	long ziehungsZahlen = 0b1111110L;
-
-	long tipp6Matches = 0b1111110L;
-	long tipp5Matches = 0b11111100L;
-	long tipp4Matches = 0b111111000L;
-	long tipp3Matches = 0b1111110000L;
-	long tipp2Matches = 0b11111100000L;
-	long tipp1Matches = 0b111111000000L;
-	long tipp0Matches = 0b1111110000000L;
-
-	// spiel77
-	int zahlSpiel77 = 7654321;
-
-	int spiel77_0_matching_end_digits = 7654320;
-	int spiel77_1_matching_end_digits = 7654301;
-	int spiel77_2_matching_end_digits = 7654021;
-	int spiel77_3_matching_end_digits = 7650321;
-	int spiel77_4_matching_end_digits = 7604321;
-	int spiel77_5_matching_end_digits = 7054321;
-	int spiel77_6_matching_end_digits = 9654321;
-	int spiel77_7_matching_end_digits = 7654321;
 
 	@BeforeEach
 	public void beforeEach() {
@@ -94,147 +69,6 @@ public class LottoUtilTest {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			LottoUtil.createTipp(1, 2, 3, 4, 5, 6, 7);
 		});
-	}
-
-	// more tests
-
-	@Test
-	public void gkl6Aus49_6_Matches_NoMatchingSuperzahl_ShouldGiveGkl2() {
-		int expected = 2;
-		int actual = LottoUtil.gkl6Aus49(ziehungsZahlen, tipp6Matches, false);
-
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gkl6Aus49_5_Matches_NoMatchingSuperzahl_ShouldGiveGkl4() {
-		int expected = 4;
-		int actual = LottoUtil.gkl6Aus49(ziehungsZahlen, tipp5Matches, false);
-
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gkl6Aus49_4_Matches_NoMatchingSuperzahl_ShouldGiveGkl6() {
-		int expected = 6;
-		int actual = LottoUtil.gkl6Aus49(ziehungsZahlen, tipp4Matches, false);
-
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gkl6Aus49_3_Matches_NoMatchingSuperzahl_ShouldGiveGkl8() {
-		int expected = 8;
-		int actual = LottoUtil.gkl6Aus49(ziehungsZahlen, tipp3Matches, false);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gkl6Aus49_0_Matches_NoMatchingSuperzahl_ShouldGiveGkl0() {
-		int expected = 0;
-		int actual = LottoUtil.gkl6Aus49(ziehungsZahlen, tipp0Matches, false);
-		assertEquals(expected, actual);
-	}
-
-	// dito für tipps mit matchender superzahl
-
-	@Test
-	public void gklSpiel77_0_MatchingEndDigits_ShouldGiveGkl0() {
-		int expected = 0;
-		int actual = LottoUtil.gklSpiel77(zahlSpiel77, spiel77_0_matching_end_digits);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gklSpiel77_1_MatchingEndDigits_ShouldGiveGkl7() {
-		int expected = 7;
-		int actual = LottoUtil.gklSpiel77(zahlSpiel77, spiel77_1_matching_end_digits);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gklSpiel77_2_MatchingEndDigits_ShouldGiveGkl6() {
-		int expected = 6;
-		int actual = LottoUtil.gklSpiel77(zahlSpiel77, spiel77_2_matching_end_digits);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gklSpiel77_3_MatchingEndDigits_ShouldGiveGkl5() {
-		int expected = 5;
-		int actual = LottoUtil.gklSpiel77(zahlSpiel77, spiel77_3_matching_end_digits);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gklSpiel77_4_MatchingEndDigits_ShouldGiveGkl4() {
-		int expected = 4;
-		int actual = LottoUtil.gklSpiel77(zahlSpiel77, spiel77_4_matching_end_digits);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gklSpiel77_5_MatchingEndDigits_ShouldGiveGkl3() {
-		int expected = 3;
-		int actual = LottoUtil.gklSpiel77(zahlSpiel77, spiel77_5_matching_end_digits);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gklSpiel77_6_MatchingEndDigits_ShouldGiveGkl2() {
-		int expected = 2;
-		int actual = LottoUtil.gklSpiel77(zahlSpiel77, spiel77_6_matching_end_digits);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void gklSpiel77_7_MatchingEndDigits_ShouldGiveGkl1() {
-		int expected = 1;
-		int actual = LottoUtil.gklSpiel77(zahlSpiel77, spiel77_7_matching_end_digits);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	void createTippShouldThrowExceptionWhenDuplicates() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			LottoUtil.createTipp(1, 1);
-		});
-	}
-
-	@Test
-	void createTippShouldThrowExceptionWhenTooMutchNumbers() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			LottoUtil.createTipp(1, 2, 3, 4, 5, 6, 7);
-		});
-	}
-
-	@Test
-	void createTippShouldThrowExceptionWhenIllegalNumbers() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			LottoUtil.createTipp(1, 2, 3, 4, 5, 6, 7);
-		});
-	}
-
-	@Test
-	void gkl6Aus49ShouldThrowExceptionWhenIllegalArguments() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			LottoUtil.gkl6Aus49(-1, tipp1, false);
-		});
-	}
-
-	@Test
-	void tippAsStringShouldDeliverStringWith6NumbersBetween1And49() {
-		long tipp = LottoUtil.createTipp();  // kommentieren
-		boolean expected = true;
-		boolean actual;
-		String tippAsString = LottoUtil.tippAsString(tipp);
-		String[] tokens = tippAsString.split(" ");
-		NavigableSet<Integer> set = new TreeSet<>();
-		for (String token : tokens) {
-			set.add(Integer.parseInt(token));
-		}
-		actual = Collections.min(set) >= 1 && Collections.max(set) <= 49 && set.size() == 6;
-		assertEquals(expected, actual);
 	}
 
 }
