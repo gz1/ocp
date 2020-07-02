5c0b1d4ae206d59cf016841235f7ca1fff0f7ecc
Günter <guenter.zaepernick@t-online.de>
2020-07-02 14:27:35 +0200

    DataInput, DataOutput

---
+++ src/main/java/de/wbstraining/ocp/io/DataOutputStreamDemo.java
@@ -0,0 +1,23 @@
+package de.wbstraining.ocp.io;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DataOutputStreamDemo {
+	public static void main(String[] args) {
+		String path = "src/main/resources/io/data_output.data";
+		try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(path))) {
+			dos.writeBoolean(true);
+			dos.writeByte(1);
+			dos.writeShort(2);
+			dos.writeInt(3);
+			dos.writeLong(4);
+			dos.writeFloat(5.0f);
+			dos.writeDouble(6.0);
+			dos.writeUTF("sieben");
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+}
