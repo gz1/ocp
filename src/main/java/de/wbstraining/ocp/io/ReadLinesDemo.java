e3f045c75acda29c05616ab0010f8ae6fb66bc9f
Günter <guenter.zaepernick@t-online.de>
2020-07-02 11:16:16 +0200

    added ReadLinesDemo

---
+++ src/main/java/de/wbstraining/ocp/io/ReadLinesDemo.java
@@ -0,0 +1,22 @@
+package de.wbstraining.ocp.io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadLinesDemo {
+	public static void main(String[] args) {
+		String path = "src/main/java/de/wbstraining/ocp/io/ReadLinesDemo.java";
+		String line;
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
