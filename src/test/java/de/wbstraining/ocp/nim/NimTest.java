ffdba80b24cd83489374240f1953af011973e23a
Günter <guenter.zaepernick@t-online.de>
2020-07-03 12:59:46 +0200

    Added NimTest

---
+++ src/test/java/de/wbstraining/ocp/nim/NimTest.java
@@ -0,0 +1,18 @@
+package de.wbstraining.ocp.nim;
+
+
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import org.junit.jupiter.api.Test;
+
+
+public class NimTest {
+
+	@Test
+	public void constructorIntIntShouldThrowExceptionIfInvalidArgs() {
+		fail("not yet implemented...");
+	}
+}
