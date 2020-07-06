763fe8f04a17ada79ccd423d1702cb8c6a293ebf
Günter <guenter.zaepernick@t-online.de>
2020-07-06 12:13:50 +0200

    added PathDemo

---
+++ src/main/java/de/wbstraining/ocp/nio/PathDemo.java
@@ -0,0 +1,21 @@
+package de.wbstraining.ocp.nio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathDemo {
+	public static void main(String[] args) {
+		String s = "src\\wbs\\nio\\PathDemo.java";
+		Path path = Paths.get(s);
+		System.out.println(path.toAbsolutePath());
+		System.out.println(path.getNameCount());
+		System.out.println(path.getRoot());
+		System.out.println(path.toAbsolutePath().getRoot());
+		System.out.println(path.getFileName());
+		System.out.println(path.getParent());
+		System.out.println(path.toUri());
+		for (Path pathElement : path) {
+			System.out.println(pathElement);
+		}
+	}
+}
