2849c1d9387a6867400cfd2902fa696d370b1c94
Günter <guenter.zaepernick@t-online.de>
2020-07-08 12:17:41 +0200

    added ResultSetnavigationDemo

---
+++ src/main/java/de/wbstraining/ocp/jdbc/ResultSetNavigationDemo.java
@@ -0,0 +1,56 @@
+package de.wbstraining.ocp.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ResultSetNavigationDemo {
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/jdbc?" + "useUnicode=yes&characterEncoding=UTF-8";
+		String user = "root";
+		String password = "mysql";
+		String sql = "SELECT * FROM Buch ORDER BY preis";
+		String isbn;
+		String autor;
+		String titel;
+		double preis;
+		try (Connection connection = DriverManager.getConnection(
+				url, user, password);
+				Statement statement = connection.createStatement(
+						ResultSet.TYPE_SCROLL_SENSITIVE,
+						ResultSet.CONCUR_READ_ONLY);
+				ResultSet rs = statement.executeQuery(sql)) {
+			rs.afterLast();
+			// von hinten nach vorne
+			while (rs.previous()) {
+				isbn = rs.getString("isbn");
+				autor = rs.getString("autor");
+				titel = rs.getString("titel");
+				preis = rs.getDouble("preis");
+				System.out.printf("%-20s%-30s%-40s%8.2f%n", isbn, autor, titel, preis);
+			}
+			System.out.println("--------------");
+			// von vorne nach hinten
+			rs.beforeFirst();
+			while (rs.next()) {
+				isbn = rs.getString("isbn");
+				autor = rs.getString("autor");
+				titel = rs.getString("titel");
+				preis = rs.getDouble("preis");
+				System.out.printf("%-20s%-30s%-40s%8.2f%n", isbn, autor, titel, preis);
+			}
+			System.out.println("--------------");
+			// absolut positionieren (zählung beginnt auch hier bei 1)
+			rs.absolute(3);
+			isbn = rs.getString("isbn");
+			autor = rs.getString("autor");
+			titel = rs.getString("titel");
+			preis = rs.getDouble("preis");
+			System.out.printf("%-20s%-30s%-40s%8.2f%n", isbn, autor, titel, preis);
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+	}
+}
