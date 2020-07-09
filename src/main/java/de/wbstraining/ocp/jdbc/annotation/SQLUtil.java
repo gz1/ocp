68cae3ddd63d8437a1ca3052750a1fa346cfcfb3
Günter <guenter.zaepernick@t-online.de>
2020-07-09 12:28:29 +0200

    update SQLUtil

--- src/main/java/de/wbstraining/ocp/jdbc/annotation/SQLUtil.java
+++ src/main/java/de/wbstraining/ocp/jdbc/annotation/SQLUtil.java
@@ -1,8 +1,11 @@
 package de.wbstraining.ocp.jdbc.annotation;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -13,16 +16,41 @@ public class SQLUtil {
 	private SQLUtilConfig config;
 
 	public SQLUtil(String pathToXML) throws IOException,
-		ParserConfigurationException, SAXException {
-		// TODO
+			ParserConfigurationException, SAXException {
+		config = new ORMXML2SQLConfig().xml2Object(pathToXML);
 	}
 
 	public String class2TableDefinition(String className, 
-		Map<String, String> tableMappings,
-		Map<String, String> typeMappings)
-			throws ClassNotFoundException, SQLException {
-		// TODO
-		return null;
+		Map<String, String> tableMappings,	Map<String, String> typeMappings)
+			throws ClassNotFoundException {
+		Class<?> cls = Class.forName(className);
+		
+		StringBuilder sql = new StringBuilder("create table if not exists ");
+		PrimaryKey primaryKey;
+		
+		NavigableMap<Integer, String> primaryKeys = new TreeMap<>();
+
+		sql.append(tableMappings.get(className)).append("(");
+
+		for (Field field : cls.getDeclaredFields()) {
+			if (field.getAnnotation(Column.class) != null) {
+				sql.append(field.getName()).append(" ")
+						.append(typeMappings
+								.get(field.getType().getSimpleName()))
+						.append(",");
+			}
+			if ((primaryKey = field.getAnnotation(PrimaryKey.class)) != null) {
+				primaryKeys.put(primaryKey.index(), field.getName());
+			}
+		}
+
+		sql.append("primary key(");
+
+		for (int i : primaryKeys.keySet()) {
+			sql.append(primaryKeys.get(i))
+					.append(i < primaryKeys.size() ? "," : "))");
+		}
+		return sql.toString();
 	}
 	
 	// create table if not exists BuchGZ1(
@@ -31,9 +59,9 @@ public class SQLUtil {
 	// )
 
 	public String class2TableDefinition(String className)
-			throws ClassNotFoundException, SQLException {
-		// TODO
-		return null;
+			throws ClassNotFoundException {
+		return class2TableDefinition(
+			className, config.getTableMappings(), config.getTypeMappings());
 	}
 
 	// erzeugt alle im descriptor aufgelisteten tabellen
