fd08eee63ccab080f4ffa4a736e2b31a53ebfeaa
Günter <guenter.zaepernick@t-online.de>
2020-07-09 09:48:39 +0200

    added ORMXML2SQLConfig

---
+++ src/main/java/de/wbstraining/ocp/jdbc/annotation/ORMXML2SQLConfig.java
@@ -0,0 +1,17 @@
+package de.wbstraining.ocp.jdbc.annotation;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ORMXML2SQLConfig extends DefaultHandler {
+
+	public SQLUtilConfig xml2Object(String pathToXML)
+			throws ParserConfigurationException, SAXException, IOException {
+			// TODO
+			return null;
+	}
+}
